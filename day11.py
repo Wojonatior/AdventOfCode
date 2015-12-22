955a40d24765103a453ca18a68adef23b5fddb72
Jarek Wojciechowski <jarekwojo@gmail.com>
2015-12-21 20:46:00 -0500

    Day11 Fix

--- day11.py
+++ day11.py
@@ -3,16 +3,27 @@
 # z = 122
 import re
 aVal,zVal = 97,122
-santasPass = "hepxcrrq"
+#Part 1 input = hepxcrrq
+#Part 2 input/part 1 answer = hepxxyzz
+santasPass = "hepxxyzz"
 check1,check2,check3,badpass = False,False,False,True
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 while (badpass):
 	#Increment String Logic Here
-
+	for i in range(1,len(santasPass)+1):
+		if(santasPass[-i] != 'z'):
+			santasPass = list(santasPass)
+			santasPass[-i] = chr(ord(santasPass[-i]) + 1)
+			santasPass = "".join(santasPass)
+			break
+		else:
+			santasPass = list(santasPass)
+			santasPass[-i] = 'a'
+			santasPass = "".join(santasPass)
 	#Checks if there is a series of 3 letters in alphabetical order
 	for i in range(len(alphabet)-2):
-		if (alphabet[i:i+2] in santasPass):
+		if (alphabet[i:i+3] in santasPass):
 			check1 = True
 	#Checks if the password contains i, o, or l
 	if ("i" not in santasPass or "o" not in santasPass or "l" not in santasPass):
@@ -20,29 +31,12 @@ while (badpass):
 	#Checks if there are two pairs of identical letters
 	match = re.match(R".*((\w)\2)(?!\2).*((\w)\4).*", santasPass)
 	if (match != None):
-		check3 = true
+		check3 = True
 	if (check1 == True and check2 == True and check3 == True):
 		badpass = False
+	else:
+		check1 = False
+		check2 = False
+		check3 = False
 print (santasPass)
 
-def incrementString(target):
-	count = len(taget) - 1
-	unIncrmented = True
-	while(unIncrmented):
-		char = target[count]
-		if(ord(char) > zVal):
-			char = "a"
-		else:
-			unIncrmented = False
-		target = target[:count] + chr(ord(char)) + target[i+1:]
-
-characters = []
-for i in range(len(santasPass)):
-	characters.append( chr((((ord(santasPass[len(santasPass)-i-1]) + 1 * (ord(santasPass[len(santasPass)-i-1])-96 // 26 ** i))-97) % 26 )+ 97) )
-#char7 = ord(c)-96 // 26 ** 1
-#char6 = ord(c)-96 // 26 ** 2
-#char5 = ord(c)-96 // 26 ** 3
-#char4 = ord(c)-96 // 26 ** 4
-#char3 = ord(c)-96 // 26 ** 5
-#char2 = ord(c)-96 // 26 ** 6
-#char1 = ord(c)-96 // 26 ** 7
