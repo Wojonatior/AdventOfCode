cf676f696a0b5b77b42f7cbe5ae46ee6c1945819
Jarek Wojciechowski <jarekwojo@gmail.com>
2016-12-22 13:22:07 -0500

    fix logic with directions, and setup pt2 solution.

--- 2016/solutions/day2.py
+++ 2016/solutions/day2.py
@@ -1,26 +1,43 @@
 with open("../input/day2input.txt") as f:
     puzzleinput = f.read()
 
-keypad = [[1,2,3],
-          [4,5,6],
-          [7,8,9]]
 t = True
 f = False
+""" pt1 keypad
+currentCoords = [1,1]
 validLocations = [[f,f,f,f,f],
                   [f,t,t,t,f],
                   [f,t,t,t,f],
                   [f,t,t,t,f],
                   [f,f,f,f,f],]
+keypad = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+"""
+"pt2 keypad"
+currentCoords = [3,0]
+keypad = [[0, 0,  1,  0, 0],
+          [0, 2,  3,  4, 0],
+          [5, 6,  7,  8, 9],
+          [0,"A","B","C",0],
+          [0, 0, "D",0,  0],]
+validLocations = [[f,f,f,f,f,f,f],
+                  [f,f,f,t,f,f,f],
+                  [f,f,t,t,t,f,f],
+                  [f,t,t,t,t,t,f],
+                  [f,f,t,t,t,f,f],
+                  [f,f,f,t,f,f,f],
+                  [f,f,f,f,f,f,f],]
+
 finalAnswer = []
-currentCoords = [2,2]
+directionDict = {
+    "D":(1,0),
+    "U":(-1,0),
+    "R":(0,1),
+    "L":(0,-1)
+}
 
 def checkMove(dir, coords):
-    directionDict = {
-        "U":(1,0),
-        "D":(-1,0),
-        "R":(0,1),
-        "L":(0,-1)
-    }
     y,x = directionDict[dir]
     newCoords = (coords[0]+y, coords[1]+x)
     return (newCoords, validLocations[newCoords[0]+1][newCoords[1]+1])
@@ -28,11 +45,10 @@ def checkMove(dir, coords):
 #remember to adjust the indicies when checking "valid locations"
 for direction in puzzleinput:
     if direction == "\n":
-        finalAnswer.append(str(keypad[ currentCoords[0] ][ currentCoords[1] ] ))
+        finalAnswer.append(str(keypad[ currentCoords[0] ][ currentCoords[1] ]))
     else:
         (newMove, validMove) = checkMove(direction, currentCoords)
         if validMove:
             currentCoords = newMove
 
-print(finalAnswer)
-
+print(str(finalAnswer))
