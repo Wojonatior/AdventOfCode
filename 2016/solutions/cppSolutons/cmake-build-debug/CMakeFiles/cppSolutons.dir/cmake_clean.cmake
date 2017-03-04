4f1adb05599763283f6da1cfac09411c949433b7
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-03-04 06:11:50 -0500

    start clion project and seperate existing solutions into seperate directory

---
+++ 2016/solutions/cppSolutons/cmake-build-debug/CMakeFiles/cppSolutons.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppSolutons.dir/main.cpp.o"
+  "cppSolutons.pdb"
+  "cppSolutons"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppSolutons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
