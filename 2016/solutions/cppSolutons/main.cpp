b62ecbd29f377e27c9892ecf317e025fb8e17634
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-03-05 01:33:27 -0500

    Complete solution for day1 part1

--- 2016/solutions/cppSolutons/main.cpp
+++ 2016/solutions/cppSolutons/main.cpp
@@ -6,6 +6,7 @@
 class Compass{
     std::vector<std::pair<int,int>> cardinalVectors;
     int currentDirection = 0;
+    std::pair<int,int> currentLocation = std::make_pair(0,0);
 public:
     // Initalizes the cardinal directions vector with pairs that represent x,y coordinate pairs
     Compass() {
@@ -20,30 +21,43 @@ public:
     std::pair<int, int> get_dir_vector(){ return cardinalVectors[currentDirection]; }
 
     //Increments or decrements the direction pointer intelligently to represent a "turn"
-    void turn (std::string turn_direction){
-        if (turn_direction == "R"){ currentDirection = (currentDirection + 1) % 4; }
-        else if (turn_direction == "L"){ currentDirection = (currentDirection - 1) % 4;}
+    void turn (char turn_direction){
+        if (turn_direction == 'R'){ currentDirection = (currentDirection + 1) % 4; }
+        else if (turn_direction == 'L'){ currentDirection = (currentDirection + 3) % 4; }
         else {std::cout << "Not sure how, but you turned a direction that isn't left or right";}
     }
+
+    void move_distance(int distance){
+        currentLocation.first += distance * cardinalVectors[currentDirection].first;
+        currentLocation.second += distance * cardinalVectors[currentDirection].second;
+    }
+
+    void apply_move(char direction, int distance){
+        turn(direction);
+        move_distance(distance);
+    }
+
+    int get_manhattan(){ return abs(currentLocation.first) + abs(currentLocation.second);}
 };
+
 
 int main() {
     Compass compass = Compass();
 
     FILE *fp;
     if( (fp = fopen("day1input.txt", "r+")) == NULL) {
-        printf("No such file\n");
+        printf("File doesn't exist\n");
         exit(1);
     }
 
     char turn_direction;
     int distance;
     fscanf(fp, "%c%d", &turn_direction, &distance);
-    std::cout << turn_direction << " " << distance << "\n";
+    compass.apply_move(turn_direction, distance);
 
-    while(fscanf(fp, ", %c%d", &turn_direction, &distance) == 2) {
-        std::cout << turn_direction << " " << distance << "\n";
-        int i = 0;
-    }
+    while(fscanf(fp, ", %c%d", &turn_direction, &distance) == 2)
+        compass.apply_move(turn_direction, distance);
+
+    std::cout << "Manhattan Distance: " << compass.get_manhattan();
     return 0;
 }
