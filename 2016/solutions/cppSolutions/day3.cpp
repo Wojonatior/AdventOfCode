3d4c9a800ff97cc1dc7169f5c155ff2645647204
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-03-10 08:41:57 -0500

    input the correct file and add a buffer to use for second half of problem

--- 2016/solutions/cppSolutions/day3.cpp
+++ 2016/solutions/cppSolutions/day3.cpp
@@ -4,6 +4,7 @@
 #include "day3.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 class triangle{
     int sides[3];
@@ -15,26 +16,36 @@ public:
     }
     bool is_valid_triangle(){
         //Check each of the sets of sides for validity
-        return (sides[0]+sides[1] > sides[2] &&
-                sides[1]+sides[2] > sides[0] &&
-                sides[2]+sides[0] > sides[1]);
+        return (sides[0] + sides[1] > sides[2] &&
+                sides[1] + sides[2] > sides[0] &&
+                sides[2] + sides[0] > sides[1]);
     }
 };
 int solve_day3(){
     FILE *fp;
-    if ((fp = fopen("day2input.txt", "r+")) == NULL) {
+    if ((fp = fopen("day3input.txt", "r+")) == NULL) {
         printf("File doesn't exist\n");
         exit(1);
     }
 
     int side1, side2, side3;
-    int valid_triangles = 0;
+    int valid_triangles_pt1 = 0;
+    int valid_triangles_pt2 = 0;
+    std::vector<int> value_buffer;
 
     while (fscanf(fp, "%d %d %d", &side1, &side2, &side3) == 3) {
-        if(triangle(side1, side2, side3).is_valid_triangle()){
-            valid_triangles++;
+        if(triangle(side1, side2, side3).is_valid_triangle()){ valid_triangles_pt1++; }
+        value_buffer.push_back(side1);
+        value_buffer.push_back(side2);
+        value_buffer.push_back(side3);
+        if(value_buffer.size() == 9){
+            if(triangle(value_buffer[0], value_buffer[3], value_buffer[6]).is_valid_triangle()){ valid_triangles_pt2++; }
+            if(triangle(value_buffer[1], value_buffer[4], value_buffer[7]).is_valid_triangle()){ valid_triangles_pt2++; }
+            if(triangle(value_buffer[2], value_buffer[5], value_buffer[8]).is_valid_triangle()){ valid_triangles_pt2++; }
+            value_buffer.clear();
         }
     }
 
-    std::cout << "Number of valid triangles: " << valid_triangles;
+    std::cout << "Number of valid triangles: " << valid_triangles_pt1;
+    std::cout << "\nNumber of valid transposed triangles: " << valid_triangles_pt2;
 }
